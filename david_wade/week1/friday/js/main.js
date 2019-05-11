ad0a828180e366722ef22d1a7c28ee25ff5e5a69
David Wade <d_wade1@icloud.com>
2019-05-12 01:01:45 +1000

    revisited week1 tuesday homework

--- david_wade/week1/friday/js/main.js
+++ david_wade/week1/friday/js/main.js
@@ -1,20 +1,41 @@
 // arrays containing objects
 
 let trainLines = {
-  nLine:['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
-  sixLine:['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'],
-  lLine:[ '8th', '6th', 'Union Square', '3rd', '1st']
-
+  'nLine':['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
+  'sixLine':['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'],
+  'lLine':[ '8th', '6th', 'Union Square', '3rd', '1st']
 
 }
 
-let numberOfStops = function(line) {
-return this.nLine.length;
+//////Counts how many stops in total for each given train line ////////
+const numberOfStops = function(lineName) {
+  let howManyStations = trainLines[lineName].length;
+  return howManyStations;
 }
 
-numberOfStops(nLine)
-console.log('number of stops in nLine is 5');
-console.log(`actual answer is ` )
+console.log(`number of stops in nLine is 6`);
+console.log(`actual answer is ${numberOfStops('nLine')}`);
+console.log(`number of stops in sixLine is 6`);
+console.log(`actual answer is ${numberOfStops('sixLine')}`);
+console.log(`number of stops in lLine is 5`);
+console.log(`actual answer is ${numberOfStops('lLine')}`);
+
+
+const singleLineTrip = function( line, startStation, endStation ){
+
+ let stationsInTrip =  trainLines[line].indexOf(endStation) - trainLines[line].indexOf(startStation);
+
+
+  return stationsInTrip;
+};
+
+// print expected output here:
+console.log('I expect to see: there should be 4 stops between 34th and 8th')
+console.log('-----------------------------------------------------------------------------')
+console.log('Trip on line N from 34th to 8th:', singleLineTrip( 'nLine', '34th', '8th' ));
+console.log('-----------------------------------------------------------------------------')
+console.log('Trip on line L from 8th to Union Square:', singleLineTrip('lLine', '8th', 'Union Square'));
+console.log('-----------------------------------------------------------------------------')
 
 
 
