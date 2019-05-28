7a516037f699bbf6e218b1b9978e138d08b8ce1a
Clara Kim <kim.y.clara@gmail.com>
2019-05-28 19:52:33 +1000

    homework for week 4 monday

---
+++ clara_kim/week4/tuesday/mta-ruby.rb
@@ -0,0 +1,28 @@
+
+
+$lines = {a:1, b:2}
+
+def single_line_trip(line, on, off)
+  puts $lines # ruby cannot see global variables from inside a function $ is for global variables
+weekend_days
+
+puts $lines
+
+single_line_trip('3'.....)
+
+
+
+
+
+
+
+
+
+
+
+def lines
+  {a:1, b:2}
+end
+
+
+functions can be called 
