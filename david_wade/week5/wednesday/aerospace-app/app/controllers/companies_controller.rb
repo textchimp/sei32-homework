620d999d5999881bfc155e9150b7a0e21d60bae1
David Wade <d_wade1@icloud.com>
2019-06-05 23:02:55 +1000

    wk5 wed company < products

--- david_wade/week5/wednesday/aerospace-app/app/controllers/companies_controller.rb
+++ david_wade/week5/wednesday/aerospace-app/app/controllers/companies_controller.rb
@@ -1,4 +1,9 @@
 class CompaniesController < ApplicationController
+
+  def new
+    @company = Company.new
+  end
+
   def index
     @companies = Company.all
   end
@@ -8,11 +13,20 @@ class CompaniesController < ApplicationController
   end
 
   def edit
+    @company = Company.find params[:id]
   end
 
   def update
+    company = Company.find params[:id]
+    company.update company_params
+
+    redirect_to(company_path( company.id ))
   end
 
   def destroy
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :headquarters, :traded_as, :founded, :revenue, :image)
   end
 end
--- david_wade/week5/wednesday/aerospace-app/config/routes.rb
+++ david_wade/week5/wednesday/aerospace-app/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  #Create
+  get
+
   #READ
   get '/companies/' => 'companies#index'
-  get '/companies/:id' => 'companies#show'
+  get '/companies/:id' => 'companies#show', as: 'company'
+
+  get '/companies/:id/edit' => 'companies#edit', as: 'company_edit'
+  patch '/companies/:id' => 'companies#update'
 
 
   # get 'companies/edit'
@@ -11,6 +17,7 @@ Rails.application.routes.draw do
   #
 
 
+  resources :companies
 
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
